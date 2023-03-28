6477437e78db19cee4dd8b9c3c4ff9c126796a22
Claudio Bustos <75331713+SK-ClaudioBustos@users.noreply.github.com>
2023-03-28 15:36:15 -0300

    modified:   src/App.js 	new file:   src/components/UpButton/UpButton.jsx 	new file:   src/components/UpButton/UpButton.scss     add:        UpButton para volver hasta arriba

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Experiencia from "./components/Experiencia/Experiencia";
 import SeccionProyectos from "./components/SectionProyectos/SectionProyectos";
 import Contacto from "./components/Contacto/Contacto";
 import Footer from "./components/Footer/Footer";
+import UpButton from "./components/UpButton/UpButton";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <SeccionProyectos/>
       <Contacto/>
       <Footer/>
+      <UpButton/>
     </div>
   );
 }
