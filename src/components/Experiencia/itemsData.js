2eac746b3c282beefc9bab7d2a0f7f1f9571ff8c
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2023-11-01 16:45:02 -0300

    feat: se cambiaron los items de algunas habilidades

--- src/components/Experiencia/itemsData.js
+++ src/components/Experiencia/itemsData.js
@@ -3,13 +3,13 @@ let data = [
         titulo:"Desarrollo Web",
         cuerpo:"Estos son mis conocimientos tecnológicos de Frontend.",
         icono:"laptop",
-        items:["HMTL", "CSS", "Javascript", "Typescript", "React", "Next js"]
+        items:["CSS", "Javascript", "Typescript", "React", "Next js"]
     },
     {
         titulo:"Otros Conocimientos",
         cuerpo:"Tambien tengo conocimientos en otras tecnologias",
         icono:"book",
-        items:["Laravel", "Livewire", "Graphql", "Nest js", "SQL"]
+        items:["Laravel", "Livewire", "Nest js","MUI Library Component"]
     },
 ]
 
