13844eab09a199349ef72e531241788d9b1ed48a
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-11-04 13:56:05 -0300

    proyecto quitado

--- src/components/SectionProyectos/proyect_data.ts
+++ src/components/SectionProyectos/proyect_data.ts
@@ -12,18 +12,18 @@ const useGetData = (): IDataRecover[] => {
     const { t } = useTranslation("global");
     const data: IDataRecover[] = [
         {
-            src: "proyecto_1.svg",
-            alt: t("proyects.p1"),
-            titulo: "Wiki",
-            git: "https://github.com/SK-ClaudioBustos/star-wars-clone-wars-website",
-            web: "https://proyect1-beta.vercel.app/"
-        },
-        {
             src: "proyecto_3.webp",
             alt: t("proyects.p3"),
             titulo: "Mu Play",
             git: "https://github.com/SK-ClaudioBustos/mu-play",
             web: "https://proyect3-lyart.vercel.app"
+        },
+        {
+            src: "proximamente.svg",
+            alt: t("proyects.default"),
+            titulo: t("proyects.default-title"),
+            git: "#",
+            web: "#"
         },
         {
             src: "proximamente.svg",
