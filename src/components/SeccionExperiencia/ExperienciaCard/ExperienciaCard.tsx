472a10a104cf15fc8a8040b8f936ed97bf9c8b61
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-11-04 13:54:30 -0300

    fix: seccion experiencia overflow

--- src/components/SeccionExperiencia/ExperienciaCard/ExperienciaCard.tsx
+++ src/components/SeccionExperiencia/ExperienciaCard/ExperienciaCard.tsx
@@ -10,14 +10,14 @@ const ExperienciaCard = (props: IExperienciaCard): JSX.Element => {
     return (
         <div className="container experiencia-container">
             <div className="row row-xxl-12 row-xl-12 row-lg-12 row-md-12 row-sm-12 experiencia-row">
-                <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 experiencia-col">
+                <div className="col-xxl-5 col-xl-5 col-lg-5 col-md-5 col-sm-12 experiencia-col">
                     <div className="ms-4">
                         <h3 className="experiencia-lugar">{lugar}</h3>
                         <h4 className="experiencia-puesto">{puesto}</h4>
                         <span className="experiencia-duracion">{duracion}</span>
                     </div>
                 </div>
-                <div className="col-xxl-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 experiencia-col">
+                <div className="col-xxl-7 col-xl-7 col-lg-7 col-md-7 col-sm-12 experiencia-col">
                     <p className="experiencia-descripcion">{descripcion}</p>
                 </div>
             </div>
