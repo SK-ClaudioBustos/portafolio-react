13f422a35e9d2f57e6f78415f6cd3794a2c05a59
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-12-04 12:44:10 -0300

    better style

--- src/components/Skill/Skill.tsx
+++ src/components/Skill/Skill.tsx
@@ -1,11 +1,14 @@
+import { useTranslation } from "react-i18next";
 import SkillItem from "../SkillItem/SkillItem";
 import useGetData from "./itemsData";
 import "./Skill.css";
 
 function Skill(): JSX.Element {
+  const { t } = useTranslation("global");
   const itemsData = useGetData();
   return (
     <section id="section-skill" className="skill seccion-clara">
+      <h2 className="skill-titulo">{t("skills.title")}</h2>
       <div className="container text-center">
         <div className="row">
           {
