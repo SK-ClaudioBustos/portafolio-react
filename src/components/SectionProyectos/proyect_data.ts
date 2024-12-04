e766b1cb0f436f772c342b71df9c93f8d7390e26
Claudio Bustos <claudiobustos.sk2000@gmail.com>
2024-12-04 10:48:57 -0300

    feat new proyect added

--- src/components/SectionProyectos/proyect_data.ts
+++ src/components/SectionProyectos/proyect_data.ts
@@ -12,18 +12,18 @@ const useGetData = (): IDataRecover[] => {
     const { t } = useTranslation("global");
     const data: IDataRecover[] = [
         {
+            src: "pokemon_wiki.webp",
+            alt: "image project pokemon wiki",
+            titulo: "Pokemon Wiki",
+            git: "https://github.com/SK-ClaudioBustos/pokemon-wiki",
+            web: "https://pokemon-wiki-mauve.vercel.app"
+        },
+        {
             src: "mu_play.webp",
             alt: "image project mu play",
             titulo: "Mu Play",
             git: "https://github.com/SK-ClaudioBustos/mu-play",
             web: "https://mu-play.vercel.app"
-        },
-        {
-            src: "proximamente.svg",
-            alt: t("proyects.default"),
-            titulo: t("proyects.default-title"),
-            git: "#",
-            web: "#"
         },
         {
             src: "proximamente.svg",
